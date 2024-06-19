73da27745abc0ef8be9fc77ce4e86e5939e14a08
Greenbueller <pennygaming10@gmail.com>
2024-06-19 04:22:50 -0400

    Test

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const pool = mysql.createPool({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.PORT || 3306,
 });
 
 const validateForm = [

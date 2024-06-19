74c56131650f9ebe236cd075e145521d2b93a1b8
Greenbueller <pennygaming10@gmail.com>
2024-06-19 16:56:24 -0400

    Data Submission! :)

--- server.js
+++ server.js
@@ -1,14 +1,83 @@
 const express = require('express');
 const mysql = require('mysql');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const session = require('express-session');
 const { check, validationResult } = require('express-validator');
 const bodyParser = require('body-parser');
 require('dotenv').config();
+const path = require('path');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true}));
+app.use(session({
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+}));
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID,
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    callbackURL: process.env.GOOGLE_CALLBACK_URL,
+},
+(accessToken, refreshToken, profile, done) => {
+    done(null, profile);
+}));
+
+passport.serializeUser((user,done) => {
+    done(null, user);
+});
+
+passport.deserializeUser((obj, done) => {
+    done(null, obj);
+});
+
+app.use(express.static(path.join(__dirname)));
+app.use('/CSS', express.static(path.join(__dirname + '/CSS')));
+app.use('/Media', express.static(path.join(__dirname + '/Media')));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+})
+
+app.get('/submit', (req, res) => {
+    res.sendFile(path.join(__dirname, 'submit.html'));
+})
+
+const isAuthenticated = (req, res, next) => {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect('/auth/google');
+};
+
+app.get('/auth/google',
+    passport.authenticate('google', { scope: ['profile', 'email'] })
+);
+
+app.get('/auth/google/callback',
+    passport.authenticate('google', { failureRedirect: '/'}),
+    (req, res) => {
+        res.sendFile(path.join(__dirname, 'submit.html'));
+    }
+);
+
+app.get('/logout', (req, res) => {
+    req.logout(() => {
+        res.redirect('/');
+    });
+});
+
+app.get('/auth/status', (req, res) => {
+    res.send({ loggedIn: req.isAuthenticated() });
+});
 
 const pool = mysql.createPool({
   connectionLimit: 10,
